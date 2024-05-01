47181210d0ba3807db8db9978abdffcecabbc858
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2024-05-01 21:31:33 +0530

    Add authentication middleware

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,19 @@ const express = require('express');
 const router = express.Router();
 
 const authRoutes = require('./auth');
+const authenticateUser = require('../middleware/authenticateUser');
+const roleFilter = require('../middleware/roleFilter');
 
 router.get('/lol', (req, res) => {
     res.send('lol');
+});
+
+// router.get('/protected', authenticateUser ,(req, res) => {
+//     res.send('protected');
+// });
+
+router.get('/protected', authenticateUser, roleFilter(["User"]) ,(req, res) => {
+    res.send('protected');
 });
 
 router.use('/auth', authRoutes);

3f0e24f567869b72e4e527a118a62e294a59c204
Sakindu Ransindu <sakindu.ransindu@gmail.com>
2024-06-09 01:14:22 +0530

    update config

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
 require("dotenv").config();
 module.exports = {
-  development: {
-    username: "root",
-    password: "1234",
-    database: "ecomdb",
-    host: "localhost",
-    dialect: process.env.DB_DIALECT || "mysql",
-    port: process.env.DB_PORT || 3306,
-    jwtSecret: "secret",
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "ecomdb",
+    "host": "localhost",
+    "dialect": process.env.DB_DIALECT || "mysql",
+    "port": process.env.DB_PORT || 3306,
+    "jwtSecret": "secret",
   },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
   },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
   },
 };
